d270dd302529ae29d6b812fd4d1b3e3a64de4da0
Cole Michaels <cmichaelsd@gmail.com>
2018-06-16 16:31:05 -0700

    test

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks("grunt-aws");
+  grunt.initConfig({
+    aws: grunt.file.readJSON("credentials.json"),
+    s3: {
+      options: {
+        accessKeyId: "<%= aws.accessKeyId %>",
+        secretAccessKey: "<%= aws.secretAccessKey %>",
+        bucket: "seller-info",
+        region: "us-west-1"
+      },
+      build: {
+        cwd: "static",
+        src: "bundle.js"
+      }
+    }
+  });
+  grunt.registerTask("default", ["s3"]);
+};
